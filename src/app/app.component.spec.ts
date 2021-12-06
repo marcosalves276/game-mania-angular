08547e213089e85f153ba520cac786a11d30443f
marcosalves <marcosalves276@outlook.com>
2021-12-06 19:53:43 -0300

    meu primeiro projeto angular

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'teste'`, () => {
+  it(`should have as title 'gamemania'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('teste');
+    expect(app.title).toEqual('gamemania');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('teste app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('gamemania app is running!');
   });
 });
